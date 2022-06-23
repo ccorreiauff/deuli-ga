89ce0f9491e2f921dda25c7039496576826b17e7
ccorreiauff <carlos_correia@id.uff.br>
2022-06-23 16:47:18 -0300

    commit 230722-1647

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,7 @@
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
+                        <a href=""></a>
                     @endif
 
                     {{ __('You are logged in!') }}
