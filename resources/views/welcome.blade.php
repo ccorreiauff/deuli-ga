7e9eab31675c5a762f1470524815a80fdf337a8f
ccorreiauff <carlos_correia@id.uff.br>
2022-06-23 17:43:49 -0300

    230722-1744

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Deuli.ga
+                                Sponsor: Deuli.ga
                             </a>
                         </div>
                     </div>
